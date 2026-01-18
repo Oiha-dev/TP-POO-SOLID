c4b86b009791e5090e000eb2c2cf298f86d793e4
Oiha_dev <cenes.gauthier@gmail.com>
2026-01-18 18:56:15 +0100

    TP SOLID : LSP

--- lsp/src/CompteBancaire.java
+++ lsp/src/CompteBancaire.java
@@ -1,8 +1,16 @@
 public class CompteBancaire {
-
     protected double solde = 100;
 
+    public boolean peutRetirer() {
+        return true;
+    }
+
     public void retirer(double montant) {
+        if (!peutRetirer()) {
+            System.out.println("Retrait impossible pour ce type de compte.");
+            System.out.println("Solde actuel : " + solde);
+            return;
+        }
         solde -= montant;
         System.out.println("Retrait effectué. Nouveau solde : " + solde);
     }
